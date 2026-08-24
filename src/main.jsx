123e68997ce8255963b003ed52f81a873088499c
alanworkspace <alanworkspace1129@gmail.com>
2026-08-24 23:13:50 +0800

    test

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Analytics } from '@vercel/analytics/react'
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize('G-T2X01E36NT')
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>,
 )
